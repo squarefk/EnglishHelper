a426b265513a51a25c5d2ae42b693eb62fdf8466
ytl <yangtianlong111@gmail.com>
2015-04-09 04:58:50 +0800

    balabala

--- src/func.cpp
+++ src/func.cpp
@@ -1,6 +1,10 @@
 #include "func.h"
 #include "word.h"
-
+#include <QFile>
+#include <QTextStream>
+#include <QDebug>
+#include <QMessageBox>
+#include <QDir>
 #include <iostream>
 #include <cstdlib>
 #include <cstdio>
@@ -11,6 +15,7 @@ std::map<QString,int> dict;
 
 Func::Func()
 {
+    loadDictionary(word);
 
 }
 
@@ -27,6 +32,7 @@ Func::~Func()
 //**********************************************
 
 QString* findSimilar(QString a){
+    return NULL;
 }
 
 QString Func::query(QString a){
@@ -40,55 +46,67 @@ QString Func::query(QString a){
 
 void Func::loadDictionary(Word* word)
 {
-	freopen("../lib/dict/dict.txt","r",stdin);
-	
+
+    qDebug()<<"vcc";
     QString cut="------------------------------";
-    QString str,a,myword;
-	char b[5000];
-	
-	//1.
-	int count=0;
-	while (gets(b)){
-		count++;
-		a=b;
-	//	if (count>1000000) break; 
-		
-		if (a==cut){
-			if (str.size()!=0){
-                while (str.size()&&str[str.size()-1]<=32) str.remove(str.size()-2,1);
-				for (int i=0;i<int(str.size());i++){
-					if (str[i]=='('||str[i]=='/') break;
-					myword+=str[i];
-				}
-                while (myword.size()&&myword[myword.size()-1]<=32) myword.remove(myword.size()-2,1);
-				
-				Word::total++;
-				word[Word::total].word=myword;
-				word[Word::total].info=str;
-				/*if (Word::total==286){
-					//cout<<str<<endl;
-					cout<<word[286].word<<endl;
-					cout<<"abashed"<<endl;
-                    QString aaa="abashed";
-					for (int i=0;i<7;i++) printf("%d %d\n",word[286].word[i],aaa[i]);
-					printf("#%d\n",word[286].word[7]);
-					cout<<"$"<<word[286].word[7]<<"$"<<endl;
-					printf("!!%d %d\n",aaa.size(),word[286].word.size());
-					//cout<<word[286].info<<endl;
-					cout<<(word[286].word=="abashed")<<endl;
-				}*/
-				str="";
-				myword="";
-			}
-		}else{
-			if (str.size()!=0) str=str+'\n';
-			str=str+a;
-		}
-	}
+    QString str,line,myword;
+
+
+    //1.
+
+    QFile *file = new QFile(":dictionary/dictionary/dictionary.txt");
+
+    if (file->open(QFile::ReadOnly))
+    {
+        qDebug()<<"open file";
+        //return;//
+        file->deleteLater();
+        QTextStream stream( file );
+        int count=0;
+        while ( !stream.atEnd() ) {
+            line = stream.readLine(); // 不包括“/n”的一行文本
+            count++;
+            if (line==cut){
+
+                if (str.size()!=0)
+                {
+                    while (str.size()&&str[str.size()-1]<=32) str.remove(str.size()-1,1);
+
+                    for (int i=0;i<int(str.size());i++){
+                        if (str[i]=='('||str[i]=='/') break;
+
+                        myword+=str[i];
+                    }
+                    while (myword.size()&&myword[myword.size()-1]<=32) myword.remove(myword.size()-1,1);
+
+                    Word::total++;
+                    word[Word::total].word=myword;
+                    word[Word::total].info=str;
+
+                    str="";
+                    myword="";
+                }
+            }else
+            {
+                if (str.size()!=0) str=str+'\n';
+                str=str+line;
+            }
+
+        }
+        file->close();
+    }else
+    {
+        qDebug()<<"no file";
+    }
+
+    qDebug()<<Word::total;
+
 	//2.
 	for (int i=1;i<=word[0].total;i++){
         dict.insert(std::pair<QString,int>(word[i].word,i));
-	}
+    }
+
+    qDebug()<<query("apple");
 }
 
 void Func::loadUser()
@@ -103,11 +121,12 @@ void Func::saveUser()
 
 QString *Func::similar(QString word)
 {
+    return NULL;
 }
 
 QString Func::randomWord()
 {
-
+    return QString("");
 }
 
 void Func::article(char *file)
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include "control.h"
-
+#include <iostream>
 #include <QApplication>
+#include <QDebug>
 using namespace std;
 
 int main(int argc, char *argv[]) {
+
     QApplication a(argc, argv);
+    //qDebug()<<QString(argv[0]);
     Control control;
     control.show();
     return a.exec();
